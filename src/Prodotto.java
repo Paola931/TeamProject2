03655c80f3966ac9d4a9732c54430969c737d45e
ToXer04 <simonefarina2004@gmail.com>
2023-10-25 17:12:18 +0200

    Restored

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -42,7 +42,7 @@ public class Prodotto {
     }
 
     public String getDescription() {
-        if (!description.isEmpty()) {
+        if (description == " ") {
             return description;
         }
         return "No description";
