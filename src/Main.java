00344993ed84bbf09a09bd457fdadae42ef9a8bb
ToXer04 <simonefarina2004@gmail.com>
2023-11-07 16:25:41 +0100

    Finita la feature:
    Un metodo che permetta di ricercare un dispositivo nel magazzino.

--- src/Main.java
+++ src/Main.java
@@ -28,4 +28,77 @@ public class Main {
         }
         System.out.println();
     }
+    public static void ricercaDispositivo(Scanner in, ArrayList<Prodotto> listaMagazzino) {
+        System.out.println("Seleziona in che modo vuoi ricercare:");
+        System.out.println("0 = esci dalla funzione di ricerca");
+        System.out.println("1 = per tipo");
+        System.out.println("2 = per produttore");
+        System.out.println("3 = per modello");
+        System.out.println("4 = per prezzo di vendita");
+        System.out.println("5 = per prezzo di acquisto");
+        System.out.println("6 = ricerca specifica per range di prezzo");
+        System.out.println();
+        String input = in.nextLine();
+        while(true) {
+            try {
+                int inputInt = Integer.parseInt(input);
+                if(inputInt >= 0 && inputInt <= 6) {
+                    break;
+                }
+                System.out.println();
+                System.out.println("Valore non supportato: " + input);
+                System.out.println();
+                System.out.println("Seleziona in che modo vuoi ricercare:");
+                System.out.println("0 = esci dalla funzione di ricerca");
+                System.out.println("1 = per tipo");
+                System.out.println("2 = per produttore");
+                System.out.println("3 = per modello");
+                System.out.println("4 = per prezzo di vendita");
+                System.out.println("5 = per prezzo di acquisto");
+                System.out.println("6 = ricerca specifica per range di prezzo");
+                System.out.println();
+                input = in.nextLine();
+            } catch (NumberFormatException nfe) {
+                System.out.println();
+                System.out.println("Valore non supportato: " + input);
+                System.out.println();
+                System.out.println("Seleziona in che modo vuoi ricercare:");
+                System.out.println("0 = esci dalla funzione di ricerca");
+                System.out.println("1 = per tipo");
+                System.out.println("2 = per produttore");
+                System.out.println("3 = per modello");
+                System.out.println("4 = per prezzo di vendita");
+                System.out.println("5 = per prezzo di acquisto");
+                System.out.println("6 = ricerca specifica per range di prezzo");
+                System.out.println();
+                input = in.nextLine();
+            }
+        }
+        System.out.println();
+        switch (input) {
+            case "0" :
+                break;
+            case "1" :
+//                ricercaTipo(in, listaMagazzino);
+                break;
+            case "2" :
+//                ricercaProduttore(in, listaMagazzino);
+                break;
+            case "3" :
+//                ricercaModello(in, listaMagazzino);
+                break;
+            case "4" :
+//                ricercaPrezzoVendita(in, listaMagazzino);
+                break;
+            case "5" :
+//                ricercaPrezzoAcquisto(in, listaMagazzino);
+                break;
+            case "6" :
+//                ricercaRangePrezzo(in, listaMagazzino);
+                break;
+            default:
+                System.out.println("Valore non supportato: " + input);
+                System.out.println();
+        }
+    }
 }
