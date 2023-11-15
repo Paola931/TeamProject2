20c116c382549322b30a6e6b7ee64ca56763ae3f
ToXer04 <simonefarina2004@gmail.com>
2023-11-15 17:48:04 +0100

    fixed a couple features

---
+++ src/Carrello.java
@@ -0,0 +1,48 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Carrello {
+    private ArrayList<Prodotto> listaCarrello;
+    private final Scanner in;
+
+    public Carrello(ArrayList<Prodotto> listaCarrello, Scanner in) {
+        this.listaCarrello = listaCarrello;
+        this.in = in;
+    }
+    public Prodotto getProdotto() {
+        System.out.println("\nScrivi l'ID del prodotto che vuoi rimuovere dal carrello:\n");
+        String input = this.in.nextLine();
+        System.out.println();
+        for (Prodotto prodotto : this.listaCarrello) {
+            if (Objects.equals(String.valueOf(prodotto.getId()), input)) {
+                return prodotto;
+            }
+        }
+        throw new NullPointerException("Non è presente un prodotto con l'id " + input + " all'interno del carrello\n");
+    }
+
+    public String rimuoviDalCarrello(Prodotto prodotto) {
+        this.listaCarrello.remove(prodotto);
+        return "Il prodotto " + prodotto + " è stato rimosso correttamente dal carrello\n";
+    }
+    public String calcoloCostoMedio() {
+        double costoTotale = 0;
+        if (!this.listaCarrello.isEmpty()) {
+            for (Prodotto prodotto : this.listaCarrello) {
+                costoTotale += prodotto.getPriceSell();
+            }
+            return String.valueOf(costoTotale / this.listaCarrello.size());
+        } else {
+            return "Il tuo carrello è vuoto";
+        }
+    }
+    @Override
+    public String toString() {
+        return "Carrello{" +
+                "listaCarello=" + listaCarrello +
+                '}';
+    }
+}
--- src/Magazzino.java
+++ src/Magazzino.java
@@ -1,19 +1,102 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Magazzino {
-    private ArrayList<Prodotto> magazzino;
-    public Magazzino(ArrayList<Prodotto> magazzino) {
-        this.magazzino = magazzino;
+    private ArrayList<Prodotto> listaMagazzino;
+    private final Scanner in;
+
+    public Magazzino(ArrayList<Prodotto> listaMagazzino, Scanner in) {
+        this.listaMagazzino = listaMagazzino;
+        this.in = in;
+    }
+    public String aggiungiAMagazzino(Prodotto prodotto) {
+        this.listaMagazzino.add(prodotto);
+        return "Il prodotto " + prodotto + " è stato aggiunto correttamente al magazzino\n";
+    }
+    public String ricercaDispositivo() {
+        System.out.println("Seleziona in che modo vuoi ricercare:");
+        System.out.println("0 = esci dalla funzione di ricerca");
+        System.out.println("1 = per tipo");
+        System.out.println("2 = per produttore");
+        System.out.println("3 = per modello");
+        System.out.println("4 = per prezzo di vendita");
+        System.out.println("5 = per prezzo di acquisto");
+        System.out.println("6 = ricerca specifica per range di prezzo");
+        System.out.println();
+        String input = in.nextLine();
+        while (true) {
+            try {
+                int inputInt = Integer.parseInt(input);
+                if (inputInt >= 0 && inputInt <= 6) {
+                    break;
+                }
+                System.out.println();
+                System.out.println("Valore non supportato: " + input);
+                System.out.println();
+                System.out.println("Seleziona in che modo vuoi ricercare:");
+                System.out.println("0 = esci dalla funzione di ricerca");
+                System.out.println("1 = per tipo");
+                System.out.println("2 = per produttore");
+                System.out.println("3 = per modello");
+                System.out.println("4 = per prezzo di vendita");
+                System.out.println("5 = per prezzo di acquisto");
+                System.out.println("6 = ricerca specifica per range di prezzo");
+                System.out.println();
+                input = in.nextLine();
+            } catch (NumberFormatException nfe) {
+                System.out.println();
+                System.out.println("Valore non supportato: " + input);
+                System.out.println();
+                System.out.println("Seleziona in che modo vuoi ricercare:");
+                System.out.println("0 = esci dalla funzione di ricerca");
+                System.out.println("1 = per tipo");
+                System.out.println("2 = per produttore");
+                System.out.println("3 = per modello");
+                System.out.println("4 = per prezzo di vendita");
+                System.out.println("5 = per prezzo di acquisto");
+                System.out.println("6 = ricerca specifica per range di prezzo");
+                System.out.println();
+                input = in.nextLine();
+            }
+        }
+        System.out.println();
+        switch (input) {
+            case "0":
+                break;
+            case "1":
+//                return ricercaTipo(this.in, this.listaMagazzino);
+                break;
+            case "2":
+//                return ricercaProduttore(this.in, this.listaMagazzino);
+                break;
+            case "3":
+//                return ricercaModello(this.in, this.listaMagazzino);
+                break;
+            case "4":
+//                return ricercaPrezzoVendita(this.in, this.listaMagazzino);
+                break;
+            case "5":
+//                return ricercaPrezzoAcquisto(this.in, this.listaMagazzino);
+                break;
+            case "6":
+//                return ricercaRangePrezzo(this.in, this.listaMagazzino);
+                break;
+            default:
+                System.out.println("Valore non supportato: " + input);
+                System.out.println();
+        }
+        return null;
     }
 
     @Override
     public String toString() {
         return "Magazzino{" +
-                "\narticolo=" + magazzino.get(0) +
-                "\n}";
+                "listaMagazzino=" + listaMagazzino +
+                '}';
     }
+
     public void printMagazzino(ArrayList<Prodotto> magazzino) {
         magazzino.forEach(System.out::println);
         System.out.println();
