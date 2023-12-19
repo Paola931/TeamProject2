a56da7642a5ad6acb67cdd2903733059a1cb0a91
ToXer04 <simonefarina2004@gmail.com>
2023-12-19 18:22:50 +0100

    Aggiunta la visualizzazione di Magazzino e Carrello, aggiunta e rimozione di articolo da Carrello e Magazzino e calcolo costo totale del Carrello

--- src/src/Carrello.java
+++ src/src/Carrello.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -29,78 +30,83 @@ public class Carrello {
         throw new NullPointerException("Non è presente un prodotto con l'id " + input + " all'interno del carrello\n");
     }
 
-    public String rimuoviDalCarrello(Prodotto prodotto) {
-        this.listaCarrello.remove(prodotto);
-        return "Il prodotto " + prodotto + " è stato rimosso correttamente dal carrello\n";
-    }
-
-    public double calcoloCostoMedio() {
-        double costoTotale = calcoloCostoTotale(this.listaCarrello);
-        return costoTotale / this.listaCarrello.size();
-    }
-
-    public static double calcoloCostoTotale(ArrayList<Prodotto> listaCarrello) {
-        double costoTotale = 0;
-        if (!listaCarrello.isEmpty()) {
-            for (Prodotto prodotto : listaCarrello) {
-                costoTotale += prodotto.getPriceSell();
+    public double calcoloCostoTotale() throws SQLException {
+        try (Connection c = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            double cost = 0;
+            Statement s = c.createStatement();
+            String q = "SELECT * FROM `Carrello`;";
+            ResultSet result = s.executeQuery(q);
+            while(result.next()) {
+                cost += result.getFloat("prezzoVendita") * result.getInt("quantità");
             }
-        } else {
-            System.out.println("Il tuo carrello è vuoto");
+            return cost;
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
         }
-        return costoTotale;
     }
-
-    public String aggiungiProdottoCarrello(int id) {
+    private int controllaQuantità(int id) throws SQLException {
         try (Connection c = DriverManager.getConnection(URL, USER, PASSWORD)) {
             Statement s = c.createStatement();
-            if (!controllaPresenzaCarrello(id)) {
+            String q1 = "SELECT * FROM `Carrello` WHERE prodottoId = '" + id + "';";
+            ResultSet result = s.executeQuery(q1);
+            if (result.next()) {
+                return result.getInt("quantità");
+            }
+            return 0;
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+    public String aggiungiProdotto(int id) {
+        try (Connection c = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            Statement s = c.createStatement();
+            if (controllaQuantità(id) > 0) {
+                String q = "UPDATE Carrello SET quantità = quantità + 1 WHERE prodottoId = '" + id + "';";
+                s.executeUpdate(q);
+            }else{
                 String q1 = "INSERT INTO `Carrello` (ProdottoId, produttore,  modello, prezzoVendita)\n" +
                         "SELECT id, produttore, modello, prezzoVendita\n" +
                         "FROM `Prodotto`\n" +
                         "WHERE id = '" + id + "';";
                 s.executeUpdate(q1);
-            }else{
-                String q = "UPDATE Carrello SET quantità = quantità + 1 WHERE prodottoId = '" + id + "';";
-                s.executeUpdate(q);
             }
         }  catch (Exception e) {
             throw new RuntimeException(e);
         }
         return "Il prodotto con id: " + id + " è stato aggiunto correttamente al carrello \n";
     }
-
-    private boolean controllaPresenzaCarrello(int id) throws SQLException {
+    public String rimuoviProdottoId(int id) {
         try (Connection c = DriverManager.getConnection(URL, USER, PASSWORD)) {
             Statement s = c.createStatement();
-            String q1 = "SELECT * FROM `Carrello` WHERE prodottoId = '" + id + "';";
-            ResultSet result = s.executeQuery(q1);
-            if (result.next()) {
-                return true;
+            if (controllaQuantità(id) > 1) {
+                String q = "UPDATE Carrello SET quantità = quantità - 1 WHERE prodottoId = '" + id + "';";
+                s.executeUpdate(q);
+            }else if (controllaQuantità(id) == 1){
+                String q = "DELETE FROM `Carrello`" + " WHERE prodottoId = '" + id + "';";
+                s.executeUpdate(q);
+            } else {
+                throw new Exception("Prodotto non presente nel carrello");
             }
-            return false;
-        } catch (SQLException e) {
-            throw new SQLException(e.getMessage());
+        }  catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "Il prodotto con id: " + id + " è stato rimosso correttamente dal carrello\n";
+    }
+
+    public void visualizza() {
+        try (Connection c = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            Statement s = c.createStatement();
+            Statement s1 = c.createStatement();
+            String q = "SELECT * FROM `Carrello`;";
+            ResultSet result = s.executeQuery(q);
+            while(result.next()) {
+                String q1 = "SELECT * FROM Prodotto" + " WHERE id = '" + result.getInt("prodottoId") + "';";
+                ResultSet result1 = s1.executeQuery(q1);
+                result1.next();
+                System.out.println("Id: " + result1.getInt("id") + ", Produttore: " + result1.getString("produttore") + ", Modello: " + result1.getString("modello") + ", Descrizione: " + result1.getString("descrizione") + ", Display: " + result1.getFloat("display") + ", Memoria: " + result1.getInt("memoria") + ", Prezzo: " + result1.getFloat("prezzoVendita") + ", Tipo: " + TipoProdotto.valueOf(result1.getString("tipo")) + ", Quantità: " + result.getInt("quantità"));
+            }
+        }  catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
-
-    /*
-    // Crea la query SQL
-            String sql = "SELECT * FROM Carrello WHERE id_prodotto = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, idProdottoDesiderato);
-
-            // Esegui la query
-            ResultSet resultSet = statement.executeQuery();
-
-            // Verifica se ci sono risultati
-            if (resultSet.next()) {
-                // Il prodotto è nel carrello
-                System.out.println("Il prodotto è nel carrello!");
-            } else {
-                // Il prodotto non è nel carrello
-                System.out.println("Il prodotto NON è nel carrello!");
-            }
-     */
-
--- src/src/MenuAdmin.java
+++ src/src/MenuAdmin.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuAdmin {
-    public static void main() {
+    public static void main() throws SQLException {
         Scanner in = new Scanner(System.in); // inizializzo Scanner
-        Magazzino magazzino = new Magazzino(new ArrayList<Prodotto>(),in);
-        Carrello carrello = new Carrello(new ArrayList<Prodotto>(),in);
+        Magazzino magazzino = new Magazzino(new ArrayList<>(),in);
+        Carrello carrello = new Carrello(new ArrayList<>(),in);
 
         //MENU PRINCIPALE
         String input = Integer.toString(1);
@@ -19,28 +19,28 @@ public class MenuAdmin {
             System.out.println("2 = Ricerca un dispositivo nel magazzino");
             System.out.println("3 = Aggiungi elemento al carrello tramite ID");
             System.out.println("4 = Rimuovi elemento dal carrello tramite ID");
-            System.out.println("5 = Visualizza i dispositivi presenti nel magazzino");
+            System.out.println("5 = Visualizza il magazzino");
             System.out.println("6 = Visualizza il carrello");
             System.out.println("7 = Visualizza il prezzo totale degli articoli presenti nel carrello");
             System.out.println("8 = Visualizza il prezzo medio degli articoli presenti nel carrello");
-            System.out.println("9 = Completa il tuo acquisto");
-            System.out.println();
+            System.out.println("9 = Completa il tuo acquisto\n");
             input = in.nextLine();
-
             switch (input) {
                 case "0": //Esci dal programma
                     System.out.println("Arrivederci e grazie per averci scelto!");
                     break;
                 case "1": //Aggiungi  un articolo al magazzino
-                    Prodotto prodotto = Prodotto.creaArticolo();
-                    System.out.println(prodotto);
+//                    TODO
+                    Prodotto p = Prodotto.creaArticolo();
+                    System.out.println(p);
                     try {
-                        System.out.println(magazzino.aggiungiAMagazzino(prodotto));
+                        System.out.println(magazzino.aggiungiAMagazzino(p));
                     } catch(Exception e) {
                         System.out.println(e.getMessage());
                     }
                     break;
                 case "2": //Ricerca un dispositivo nel magazzino
+//                    TODO
                     try {
                         System.out.println(magazzino.ricercaDispositivo());
                     } catch (Exception e) {
@@ -48,32 +48,44 @@ public class MenuAdmin {
                     }
                     break;
                 case "3": // Aggiungi elemento al carrello tramite ID
+//                    TODO: DONE
                     try {
                         System.out.println("Inserisci l'ID del prodotto che desideri aggiungere al carrello");
                         int id = in.nextInt();
                         String inp = in.nextLine();
-                        System.out.println(carrello.aggiungiProdottoCarrello(id));
+                        System.out.println(magazzino.rimuoviProdotto(id));
+                        System.out.println(carrello.aggiungiProdotto(id));
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
                     break;
                 case "4": // Rimuovi elemento dal carrello tramite ID
+//                    TODO: DONE
                     try {
-                        prodotto = carrello.getProdotto();
-                        carrello.rimuoviDalCarrello(prodotto);
-                        magazzino.aggiungiAMagazzino(prodotto);
-                    } catch(NullPointerException | SQLException npe) {
-                        System.out.println(npe);
+                        System.out.println("Inserisci l'ID del prodotto che desideri rimuovere dal carrello");
+                        int id = in.nextInt();
+                        String inp = in.nextLine();
+                        System.out.println(carrello.rimuoviProdottoId(id));
+                        System.out.println(magazzino.aggiungiProdottoId(id));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
                     }
                     break;
                 case "5": // Visualizza i dispositivi presenti nel magazzino
-//                    magazzino.printMagazzino(listaMagazzino);
+//                    TODO: DONE | ADD JOIN?
+                    magazzino.visualizza();
                     break;
                 case "6": // Visualizza il carrello
-//                    carrello.printCarrello(listaCarrello);
+//                    TODO DONE | ADD JOIN?
+                   carrello.visualizza();
                     break;
                 case "7": // Visualizza il prezzo totale degli articoli presenti nel carrello
-//                    System.out.println(calcoloCostoTotale(listaCarrello));
+//                    TODO
+                    System.out.println(carrello.calcoloCostoTotale());
+                    break;
+                case "8": // Visualizza il prezzo medio degli articoli presenti nel carrello
+//                    TODO
+//                    System.out.println(calcoloCostoMedio(listaCarrello));
                     break;
             }
         }
